773a6460db0b0291de01bfd7ae04e0ae8ea71453
LukeDonmoyer <lukedonmoyer@gmail.com>
2021-02-11 17:20:33 -0500

    Added channel tests

--- src/channel.py
+++ src/channel.py
@@ -29,16 +29,18 @@ class Channel:
 
     def next_log_interval(self):
         """
-        Generator that returns the time in seconds to wait until the next log.
+        Method that returns the time in seconds to wait until the next log.
         Default is 5 minutes.
         Can be a floating point number for milliseconds.
         """
         return 60 * 5
 
     def scan(self):
-        """This method generates the actual value to be logged. The return type should be a dict with string keys and
-        values that are serializable. Any exceptions that occur while this method is running will be caught and
-        logged in place of a data value."""
+        """
+        This method generates the actual value to be logged. The return type should be a dict with string keys and
+        values that are serializable or a tuple containing the value dict and a list of any faults that were generated.
+        Any exceptions that occur while this method is running will be caught and logged in place of a data value.
+        """
         pass
 
     def log(self):
@@ -47,10 +49,11 @@ class Channel:
             value = self.scan()
 
             # Add value to cache and pop if cache has reached max size
-            if len(self.cache) >= self.cache_size:
-                self.cache.pop()
-            self.cache.insert(0, value)
+            if self.cache_size > 0:
+                if len(self.cache) >= self.cache_size:
+                    self.cache.pop()
+                self.cache.insert(0, value)
 
             return value
         except Exception as e:
-            raise e
+            return {}, [repr(e)]
