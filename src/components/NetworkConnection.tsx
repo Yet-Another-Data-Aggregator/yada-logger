d1601bb00d38992bf98e87e5df3864c6492251b3
Steven Smiley <smileysj1@gcc.edu>
2021-02-14 12:20:24 -0500

    Migrating to desktop to continue work

--- src/components/NetworkConnection.tsx
+++ src/components/NetworkConnection.tsx
@@ -1,9 +1,20 @@
 import React from "react";
-import { List } from "reactstrap";
+import {List, ListItem, ListItemText, Icon, ListItemIcon} from "@material-ui/core";
+import {Wifi} from "@material-ui/icons";
 
 export default function NetworkConnection() {
 
     return (
-        
+        <List className="w-1/2 border">
+            <ListItem className="border">
+                <ListItemIcon>
+                    <Wifi/>
+                </ListItemIcon>
+                <ListItemText className="flex justify-center" primary="This is where"/>
+            </ListItem>
+            <ListItemText primary="my wifi networks"/>
+            <ListItemText primary="would be displayed"/>
+            <ListItemText primary="if I could find any!"/>
+        </List>
     );
 }
