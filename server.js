16b8bb2e6c5ce4b4ddd10b5fb03feee78301b765
Steven Smiley <smileysj1@gcc.edu>
2021-02-15 18:54:56 -0500

    Fixed something funky

--- server.js
+++ server.js
@@ -49,32 +49,30 @@ app.post("/enable_wifi", function(request, response) {
       wifi_passcode:  request.body.wifi_passcode,
   };
 
-//rescan wifi
-app.get("/rescan_wifi", function(request, response) {
-    console.log("Server got /rescan_wifi");
-    iwlist(function(error, result) {
-        log_error_send_success_with(result[0], error, response);
-    });
-});
-
   // TODO: If wifi did not come up correctly, it should fail
   // currently we ignore ifup failures.
-  wifi_manager.enable_wifi_mode(conn_info, function(error) {
-      if (error) {
-          console.log("Enable Wifi ERROR: " + error);
-          console.log("Attempt to re-enable AP mode");
-          wifi_manager.enable_ap_mode(config.access_point.ssid, function(error) {
-              console.log("... AP mode reset");
-          });
-          response.redirect("/");
-      }
-      // Success! - exit
-      console.log("Wifi Enabled! - Exiting");
-      process.exit(0);
-  });
+wifi_manager.enable_wifi_mode(conn_info, function(error) {
+    if (error) {
+        console.log("Enable Wifi ERROR: " + error);
+        console.log("Attempt to re-enable AP mode");
+        wifi_manager.enable_ap_mode(config.access_point.ssid, function(error) {
+            console.log("... AP mode reset");
+        });
+        response.redirect("/");
+    }
+    // Success! - exit
+    console.log("Wifi Enabled! - Exiting");
+    process.exit(0);
+});
 });
 
-
+//route handler to rescan wifi
+app.get("/rescan_wifi", function(request, response) {
+  console.log("Server got /rescan_wifi");
+  iwlist(function(error, result) {
+      log_error_send_success_with(result[0], error, response);
+  });
+});
 
 //Check if we are currently connected to a wifi network
 wifi_manager.is_wifi_enabled(function(error, result_ip) {		
