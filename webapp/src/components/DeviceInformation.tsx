0ced3a1a7917096a0becec7a97e4a6dc0b36eaf1
Steven Smiley <36318270+smileysj1@users.noreply.github.com>
2021-02-22 13:38:18 -0500

    Added prettier config file.

--- webapp/src/components/DeviceInformation.tsx
+++ webapp/src/components/DeviceInformation.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, useState } from 'react';
 import {
     Grid,
     Select,
@@ -7,65 +7,86 @@ import {
     TextField,
     Card,
     MenuItem,
-  } from "@material-ui/core";
-import { Info, PermDeviceInformation } from "@material-ui/icons";
+} from '@material-ui/core';
+import { Info, PermDeviceInformation } from '@material-ui/icons';
 
 export default function DeviceInformation() {
-
-    const [deviceInfo, setDeviceInfo] : [any, any] = useState(null);
+    const [deviceInfo, setDeviceInfo]: [any, any] = useState(null);
+    const [equipmentTypeList, setEquipmentTypeList]: [any, any] = useState(
+        null
+    );
     const [equipmentType, setEquipmentType] = useState('');
 
-    const handleEquipmentChange = (e: ChangeEvent<{value: unknown}>) => {
+    const handleEquipmentChange = (e: ChangeEvent<{ value: unknown }>) => {
         setEquipmentType(e.target.value as string);
-    }
+    };
 
-    function getDeviceInfo(){    
-        if(!deviceInfo) {
-    fetch("/devinfo")
-      .then(async (response) => {
-        var responseJson = await response.json();
-        const ipv4info = responseJson[0];
+    function getDeviceInfo() {
+        if (!deviceInfo) {
+            fetch('/devinfo')
+                .then(async (response) => {
+                    var responseJson = await response.json();
+                    const ipv4info = responseJson[0];
 
-        console.log(ipv4info);
-        setDeviceInfo(ipv4info);
-      })
-      .catch((reason) => {
-        console.log("Something went wrong: " + reason);
-      });
-    }
+                    console.log(ipv4info);
+                    setDeviceInfo(ipv4info);
+                })
+                .catch((reason) => {
+                    console.log('Something went wrong: ' + reason);
+                });
+        }
     }
 
     getDeviceInfo();
 
+    //TODO: Get equipment types from firestore.
+
     return (
         <Container className="w-11/12">
-        <Grid container direction="column" alignItems='flex-start' spacing={2}>
-            <Grid item>Logger Settings</Grid>
-            <Grid item>IP: {deviceInfo?.address ?? "<unknown>"}
-                <Info className="pl-1"/>
-            </Grid>
+            <Grid
+                container
+                direction="column"
+                alignItems="flex-start"
+                spacing={2}
+            >
+                <Grid item className="text-2xl font-bold">
+                    Logger Settings
+                </Grid>
+                <Grid item>
+                    IP: {deviceInfo?.address ?? '<unknown>'}
+                    <Info className="pl-1" />
+                </Grid>
 
-            <Grid item>MAC: {deviceInfo?.mac ?? "<unknown>"}
-                <PermDeviceInformation className="pl-1"/>
-            </Grid>
+                <Grid item>
+                    MAC: {deviceInfo?.mac ?? '<unknown>'}
+                    <PermDeviceInformation className="pl-1" />
+                </Grid>
 
-            <Grid item>
-            <TextField variant='outlined' label="Device Name"/>
-            </Grid>
+                <Grid item>
+                    <TextField variant="outlined" label="Device Name" />
+                </Grid>
 
-            <Grid item>
-                <TextField variant='outlined' label="Site ID (optional)"/>
-            </Grid>
+                <Grid item>
+                    <TextField variant="outlined" label="Site ID (optional)" />
+                </Grid>
 
-            <Grid item>
-            <Select onChange={handleEquipmentChange} value={equipmentType}>
-                <MenuItem value={'A'}>Equipment A</MenuItem>
-                <MenuItem value={'B'}>Equipment B</MenuItem>
-                <MenuItem value={'C'}>Equipment C</MenuItem>
-            </Select>
+                <Grid item>
+                    <Select
+                        onChange={handleEquipmentChange}
+                        value={equipmentType}
+                    >
+                        {equipmentTypeList ? (
+                            equipmentTypeList?.map((e: string, index: any) => {
+                                return (
+                                    <MenuItem value={e}>Equipment {e}</MenuItem>
+                                );
+                            })
+                        ) : (
+                            <MenuItem>No Equipment Types</MenuItem>
+                        )}
+                    </Select>
+                </Grid>
             </Grid>
-
-        </Grid>
         </Container>
     );
 }
