50b75d456c305d61f0dfac7016c24833493dec12
Steven Smiley <36318270+smileysj1@users.noreply.github.com>
2021-02-22 21:37:46 -0500

    Integrated navbar.

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,26 +1,67 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import NetworkConnection from "./components/NetworkConnection";
-import DeviceInformation from "./components/DeviceInformation";
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    useHistory,
+} from 'react-router-dom';
+import { Animated } from 'react-animated-css';
+import logo from './logo.svg';
+import './App.scss';
+import NetworkConnection from './components/NetworkConnection';
+import DeviceInformation from './components/DeviceInformation';
+import StaticNavbar, { StaticNavItem } from './components/StaticNavbar';
+import { Info, Wifi } from '@material-ui/icons';
 
 function App() {
-  // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
-  const callBackendAPI = async () => {
-    const response = await fetch("/express_backend");
-    const body = await response.json();
+    // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+    const callBackendAPI = async () => {
+        const response = await fetch('/express_backend');
+        const body = await response.json();
 
-    if (response.status !== 200) {
-      throw Error(body.message);
-    }
-    return body;
-  };
+        if (response.status !== 200) {
+            throw Error(body.message);
+        }
+        return body;
+    };
 
-  return (
-    <div className="App">
-      <DeviceInformation />
-    </div>
-  );
+    return (
+        <Router>
+            {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+            <Switch>
+                <Route path="/">
+                    <Animated
+                        animationIn="fadeIn"
+                        animationOut="fadeOut"
+                        isVisible={true}
+                    >
+                        <div className="app">
+                            <StaticNavbar
+                                autoCollapse={true}
+                                roundRightCorners={true}
+                            >
+                                <StaticNavItem
+                                    label={'Network Connection'}
+                                    route={'/network-connection'}
+                                    icon={<Wifi />}
+                                >
+                                    <NetworkConnection />
+                                </StaticNavItem>
+                                <StaticNavItem
+                                    label={'Device Information'}
+                                    route={'/device-information'}
+                                    icon={<Info />}
+                                >
+                                    <DeviceInformation />
+                                </StaticNavItem>
+                            </StaticNavbar>
+                        </div>
+                    </Animated>
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
