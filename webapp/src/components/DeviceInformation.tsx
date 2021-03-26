4ca4758ca6da028f84ccd6d2d70478f92c4d7d33
Steven Smiley <36318270+smileysj1@users.noreply.github.com>
2021-03-25 20:10:58 -0400

    Re-enabled attempt connection.

--- webapp/src/components/DeviceInformation.tsx
+++ webapp/src/components/DeviceInformation.tsx
@@ -100,7 +100,7 @@ export default function DeviceInformation() {
             const passkey = searchParams.get('passkey');
 
             if (ssid != null && passkey != null && !isBlank(ssid)) {
-                //attemptConnection(ssid, passkey);
+                attemptConnection(ssid, passkey);
             } else {
                 alert(
                     'SSID or network passkey not specified.  Return to network selection and try again.'
